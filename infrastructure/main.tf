222869014bba14d72c7e9bb8e5a8da68308e6afd
Aakanksha <aka.pharande@gmail.com>
2025-03-07 22:58:03 -0500

    Adding formatting errors

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,15 +1,11 @@
 provider "azurerm" {
   features
-  subscription_id = "45877b2c-82aa-41e4-8da1-8a4ebb66eaad"
-
+  subscription_id=  "45877b2c-82aa-41e4-8da1-8a4ebb66eaad"
 }
 
-resource "azurerm_resource_group" "rg" {name="myResourceGroup"location = "East US"}
+resource "azurerm_resource_group" "rg" { name="myResourceGroup"location = East US}
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "aakankshasa"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+resource "azurerm_storage_account" "storage" {name="aakankshasa"
+resource_group_name=azurerm_resource_group.rg.name
+location=azurerm_resource_group.rg.location account_tier="Standard"
+account_replication_type= "LRS"}
