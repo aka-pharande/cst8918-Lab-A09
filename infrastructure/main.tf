3fc330559aa1b07ce96fd967a9bfbcf16af7327f
Aakanksha <aka.pharande@gmail.com>
2025-03-07 23:42:18 -0500

    Updated tf file for validation  errors

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,16 @@
+terraform {
+  required_version = ">= 1.0"  # Specify the required Terraform version
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"  # Provider source
+      version = "~> 2.0"             # Specify the provider version
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "45877b2c-82aa-41e4-8da1-8a4ebb66eaad"
-
 }
 
 resource "azurerm_resource_group" "rg" {
