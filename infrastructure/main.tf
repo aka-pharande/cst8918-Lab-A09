db656129009cdd0cf7ab3ea0f0457db7962b2b35
Aakanksha <aka.pharande@gmail.com>
2025-03-07 23:23:29 -0500

    Updated tf file for validation errors

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,28 @@
-provider "azurerm" {
-  features
-  subscription_id=  "45877b2c-82aa-41e4-8da1-8a4ebb66eaad"
+terraform {
+  required_version = ">= 1.0"  
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"  
+    }
+  }
 }
 
-resource "azurerm_resource_group" "rg" { name="myResourceGroup"location = East US}
+provider "azurerm" {
+  features {}
+  subscription_id = "45877b2c-82aa-41e4-8da1-8a4ebb66eaad"
+}
 
-resource "azurerm_storage_account" "storage" {name="aakankshasa"
-resource_group_name=azurerm_resource_group.rg.name
-location=azurerm_resource_group.rg.location account_tier="Standard"
-account_replication_type= "LRS"}
+resource "azurerm_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "aakankshasa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
